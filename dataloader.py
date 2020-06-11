10706cbfe6d452402665480ea0bc37fc81f1935f
Zhniing <690710205@qq.com>
2020-06-11 16:03:51 +0800

    Update dataloader(add slice), add optimizer

--- dataloader.py
+++ dataloader.py
@@ -3,14 +3,14 @@ import skimage.io as io
 import torch
 
 
-def load_train_data(data_path):
+def load_train_data(data_path: str):
     t1_path = glob.glob(data_path + '/*T1.img')
     t2_path = glob.glob(data_path + '/*T2.img')
     gt_path = glob.glob(data_path + '/*label.img')  # ground truth
 
-    list_t1 = []
-    list_t2 = []
-    list_gt = []
+    patch_t1 = []
+    patch_t2 = []
+    patch_gt = []
 
     for i in range(len(t1_path)):
         img_t1 = t1_path[i]  # type : string
@@ -21,13 +21,22 @@ def load_train_data(data_path):
         t2 = io.imread(img_t2, plugin='simpleitk')
         gt = io.imread(img_gt, plugin='simpleitk')
 
-        t1 = torch.tensor(t1, dtype=torch.float32)  # tyep : Tensor
+        t1 = torch.tensor(t1, dtype=torch.float32)  # tyep : torch.Tensor
         t2 = torch.tensor(t2, dtype=torch.float32)
         gt = torch.tensor(gt, dtype=torch.float32)
 
-        list_t1.append(t1)  # type : list of Tensor
-        list_t2.append(t2)
-        list_gt.append(gt)
+        C, H, W = t1.shape
+        patch_size = 64
+
+        for c in range(0, C, int((C-patch_size)/4)):
+            if c+patch_size > C: break
+            for h in range(0, H, int((H-patch_size)/4)):
+                if h + patch_size > H: break
+                for w in range(0, W, int((W-patch_size)/4)):
+                    if w + patch_size > W: break
+                    patch_t1.append(t1[c:c+patch_size, h:h+patch_size, w:w+patch_size])
+                    patch_t2.append(t2[c:c+patch_size, h:h+patch_size, w:w+patch_size])
+                    patch_gt.append(gt[c:c+patch_size, h:h+patch_size, w:w+patch_size])
 
     print('load train data successfully')
-    return list_t1, list_t2, list_gt
+    return patch_t1, patch_t2, patch_gt
--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from network import *
-import torch
 from dataloader import *
+import torch.optim as optim
 
 data_path = '../datasets/iseg2017/'
-list_t1, list_t2, list_gt = load_train_data(data_path)
+patch_t1, patch_t2, patch_gt = load_train_data(data_path)
 
-for i in range(len(list_t1)):
-    t1 = list_t1[i]
-    t2 = list_t2[i]
-    gt = list_gt[i]
+model = Unet(in_ch=2, out_ch=4).cuda(0)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+for i in range(len(patch_t1)):
+    print(i)
+    t1 = patch_t1[i]
+    t2 = patch_t2[i]
+    gt = patch_gt[i]
 
     t1 = t1.unsqueeze(dim=1)
     t2 = t2.unsqueeze(dim=1)
 
-    img = torch.cat((t1, t2), dim=1).to('cuda')
+    image = torch.cat((t1, t2), dim=1).to(device='cuda')
+    # print(torch.cuda.max_memory_allocated())
 
-    U_net = Unet(in_ch=2, out_ch=4).to('cuda')
-    out = U_net(img)
+    output = model(image)
+
+    loss = F.cross_entropy(output, gt)
+    loss.backward()  # 误差反向传播 : 计算梯度
+    optimizer.step()  # 更新参数, 在梯度计算后使用
+
+    torch.cuda.empty_cache()
